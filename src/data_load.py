9bdbc790e81971f1145f5621b14a9a8a3451c5d8
Maksym Lirko <maksym.lirko@gmail.com>
2026-03-10 13:09:06 +0200

    Update CI file and py files

--- src/data_load.py
+++ src/data_load.py
@@ -1,24 +1,18 @@
 import pandas as pd
-import requests
 import os
 
 
 def download_data():
     url = "./data/raw/tz_opendata_z01012024_01022024.csv"
-
     output_path = "data/raw/vehicle_registrations.csv"
-
     print(f"Починаю завантаження даних з {url}...")
 
     try:
         df = pd.read_csv(url, sep=';', encoding='utf-8', low_memory=False)
-
         os.makedirs(os.path.dirname(output_path), exist_ok=True)
-
         df.to_csv(output_path, index=False)
         print(f"Дані успішно збережено у {output_path}")
         print(f"Завантажено рядків: {len(df)}")
-
         print("\nПерші 5 рядків даних:")
         print(df.head())
 
