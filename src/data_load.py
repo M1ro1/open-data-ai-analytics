513b7a59565a64e322767277684aa27e4b53fb18
Maksym <114308474+M1ro1@users.noreply.github.com>
2026-04-13 13:25:16 +0300

    Implement data loading into database from CSV
    Refactored data loading process to use a database and removed the download_data function.

--- src/data_load.py
+++ src/data_load.py
@@ -1,24 +1,22 @@
 import pandas as pd
+from sqlalchemy import create_engine
 import os
 
+def load_data():
+    input_path = "/app/data/sample/sample_data.csv"
+    db_path = "sqlite:////app/db/database.db"
 
-def download_data():
-    url = "./data/sample/sample_data.csv"
-    output_path = "data/sample/vehicle_registrations.csv"
-    print(f"Починаю завантаження даних з {url}...")
-
+    print(f"Починаю завантаження даних з {input_path}...")
     try:
-        df = pd.read_csv(url, sep=';', encoding='utf-8', low_memory=False)
-        os.makedirs(os.path.dirname(output_path), exist_ok=True)
-        df.to_csv(output_path, index=False)
-        print(f"Дані успішно збережено у {output_path}")
-        print(f"Завантажено рядків: {len(df)}")
-        print("\nПерші 5 рядків даних:")
-        print(df.head())
-
+        df = pd.read_csv(input_path, sep=';', encoding='utf-8', low_memory=False)
+        os.makedirs("/app/db", exist_ok=True)
+        
+        engine = create_engine(db_path)
+        df.to_sql('vehicles', engine, if_exists='replace', index=False)
+        
+        print(f"Дані успішно збережено у БД! Завантажено рядків: {len(df)}")
     except Exception as e:
         print(f"Помилка при завантаженні: {e}")
 
-
 if __name__ == "__main__":
-    download_data()
+    load_data()
